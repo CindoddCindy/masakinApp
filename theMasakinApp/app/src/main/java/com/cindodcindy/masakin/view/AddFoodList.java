e54fb49110141de0068452c39cb9340620d59392
CindoddCindy <cindodcindy@gmail.com>
2021-06-23 15:57:44 +0700

    tambha data

---
+++ theMasakinApp/app/src/main/java/com/cindodcindy/masakin/view/AddFoodList.java
@@ -0,0 +1,16 @@
+package com.cindodcindy.masakin.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cindodcindy.masakin.R;
+
+public class AddFoodList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_food_list);
+    }
+}
---
+++ theMasakinApp/app/src/main/java/com/cindodcindy/masakin/view/FoodList.java
@@ -0,0 +1,16 @@
+package com.cindodcindy.masakin.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cindodcindy.masakin.R;
+
+public class FoodList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_list);
+    }
+}
